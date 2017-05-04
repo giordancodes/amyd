8edc664c2f3b9c23592694eb32746efd4a22eea5
Giordan Battaglin <gio@giordan.ca>
2017-05-04 00:01:49 -0400

    initial structure & styles

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import './amyd.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="full-wrap">
+        <header>
+          <div className="logo-container">
+            <img src={logo} className="logo" alt="logo" />
+          </div>
+        </header>
+        <nav>
+          <ul>
+            <li>123</li>
+            <li>123</li>
+            <li>123</li>
+          </ul>
+        </nav>
       </div>
     );
   }
